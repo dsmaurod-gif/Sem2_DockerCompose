c4998e9d5e302c35f297fc3488075d5f575d3cca
DIEGO <dsmaurod@gmail.com>
2026-09-12 15:52:58 -0500

    feat: Creacion del docker-compose

--- api/src/config.js
+++ api/src/config.js
@@ -23,7 +23,7 @@ class Config {
         return port;
     }
     get message() {
-        return 'Diego Mauricio' || process.env.MESSAGE || this.DEFAULT_MESSAGE;
+        return process.env.MESSAGE || this.DEFAULT_MESSAGE;
     }
     get logLevel() {
         let logLevel = process.env.LOG_LEVEL || this.DEFAULT_LOG_LEVEL;
